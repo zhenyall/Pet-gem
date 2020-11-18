0b7c646921ea22a198ceaf16ebc0709fcda386de
Zhenya <zhenya.lugovskoi95@gmail.com>
2020-11-18 18:02:03 +0200

    first commit

---
+++ lib/pet-gem.rb
@@ -0,0 +1,26 @@
+class Pet
+	def initialize
+		@pet = pet
+	end
+
+	def to_html
+		comtent = File.read("#{self.class.root}/pet-gem.html")
+		@html_new = "#{self.class.root}/pet-gem.html"
+		File.open(@html_new, 'w') {|f|
+			content.gsub!('{{pet}}', @pet.class.to_s)
+			content.gsub!('{{name}}', @pet.name)
+			content.gsub!('{{emoji}}', @pet.emoji) 
+      content.gsub!('{{response}}', @pet.response.join('<br>')) 
+      f.puts content
+    	}
+  end
+
+  def open_html
+    to_html    
+    system("xdg-open #{@html_new}")
+  end
+
+  def self.root
+    File.expand_path '..', __FILE__
+  end
+end
---
+++ pet-gem.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'Pet-gem'
+  s.version     = '0.0.1'
+  s.date        = '2020-11-16'
+  s.summary     = "Hola!"
+  s.description = "Gems and pets in HTML"
+  s.authors     = ["Zhenya Lugovskoi"]
+  s.email       = 'zhenya.lugovskoi95@gmail.com'
+  s.files       = ["lib/pet-gem.rb"]
+  s.homepage    =
+    'https://github.com/zhenyall/Pet-gem'
+  s.license       = 'MIT'
+end
